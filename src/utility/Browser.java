cb47f38886664f6ad3fb5622914887d038bd79a2
ivanxue <52zhizhi>
2017-08-28 14:47:08 +1000

    htmlunit

--- src/utility/Browser.java
+++ src/utility/Browser.java
@@ -5,8 +5,10 @@ package utility;
  */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
