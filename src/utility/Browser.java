22709ee8de31a47cf616a09bb84bb9e08c6fcc3e
Ivan Xue <xue.ziheng@gmail.com>
2018-03-19 08:52:03 +1100

    Appium Integration

--- src/utility/Browser.java
+++ src/utility/Browser.java
@@ -3,6 +3,7 @@ package utility;
 /**
  * Created by ivanxue on 11/08/2017.
  */
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,7 +20,7 @@ import java.net.URL;
 public class Browser {
 
     public static WebDriver driver;
-    
+
     // remote server configuration in browserstack
     public static final String USERNAME = "feng20";
     public static final String AUTOMATE_KEY = "bxxkpf34hS1BKKyciXr2";
@@ -29,33 +30,42 @@ public class Browser {
 
     // please download and change the driver name based on your system
     public static WebDriver getBrowser(String browserType) throws MalformedURLException {
+        String osName = System.getProperty("os.name");
         switch (browserType) {
             case "firefox":
-            System.setProperty("webdriver.gecko.driver", Constant.driverPath + "geckodriver");
-            return	driver = new FirefoxDriver();
+                if (osName.contains("Mac")) {
+                    System.setProperty("webdriver.gecko.driver", Constant.driverPath + "geckodriver");
+                } else if (osName.contains("Windows")) {
+                    System.setProperty("webdriver.gecko.driver", Constant.driverPath + "geckodriver.exe");
+                }
+                return driver = new FirefoxDriver();
             case "chrome":
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("no-sandbox");
-            System.setProperty("webdriver.chrome.driver", Constant.driverPath + "chromedriver");
-            return	driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("no-sandbox");
+                if (osName.contains("Mac")) {
+                    System.setProperty("webdriver.chrome.driver", Constant.driverPath + "chromedriver");
+                } else if (osName.contains("Windows")) {
+                    System.setProperty("webdriver.chrome.driver", Constant.driverPath + "chromedriver.exe");
+                }
+                return driver = new ChromeDriver();
             case "htmlunit":
-            return	driver = new HtmlUnitDriver();
+                return driver = new HtmlUnitDriver();
             case "safari":
-            return	driver = new SafariDriver();
+                return driver = new SafariDriver();
             case "edge":
-            System.setProperty("webdriver.edge.driver", Constant.driverPath + "MicrosoftWebDriver.exe");
-            return	driver = new EdgeDriver();
+                System.setProperty("webdriver.edge.driver", Constant.driverPath + "MicrosoftWebDriver.exe");
+                return driver = new EdgeDriver();
             case "Remote":
-            caps.setCapability("browser", "Firefox");
-            caps.setCapability("browser_version", "48.0");
-            caps.setCapability("os", "Windows");
-            caps.setCapability("os_version", "10");
-            caps.setCapability("browserstack.debug", "true");
+                caps.setCapability("browser", "Firefox");
+                caps.setCapability("browser_version", "48.0");
+                caps.setCapability("os", "Windows");
+                caps.setCapability("os_version", "10");
+                caps.setCapability("browserstack.debug", "true");
 
-            return driver = new RemoteWebDriver(new URL(URL), caps);
+                return driver = new RemoteWebDriver(new URL(URL), caps);
             default:
-            System.out.println("browser: " + browserType + " is invalid, Launching Firefox as browser of choice..");
-            return driver = new FirefoxDriver();
+                System.out.println("browser: " + browserType + " is invalid, Launching Firefox as browser of choice..");
+                return driver = new FirefoxDriver();
         }
     }
 }
